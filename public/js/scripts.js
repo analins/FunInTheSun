a9eec85d6b18b95ed3ec75f63f6ab4ae6de0ca1e
Nat Carlson <naca@natcarlson.com>
2016-01-17 17:10:49 -0500

    creates user-manager partial, adds template anchor to index.ejs, adds functions to scripts.js-createUser, setCreateUserFormHandler, logInUser, setLogInFormHandler

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,73 @@
 console.log(.....loaded.....);
 
+// Create a new user in db
+function createUser(userData, callback){
+  $.ajax({
+    method: 'post',
+    url: '/api/users',
+    data: {user: userData},
+    success: function(data){
+      callback(data);
+    }
+  });
+}
+
+// New user sign up form
+function setCreateUserFormHandler(){
+  $('form#sign-up').on('submit', function(e) {
+    e.preventDefault();
+
+    var usernameField = $(this).find('input[name="username"]');
+    var usernameText = usernameField.val();
+    usernameField.val('');
+
+    var passwordField = $(this).find('input[name="password"]');
+    var passwordText = passwordField.val();
+    passwordField.val();
+
+    var userData = {username: usernameText, passwordText};
+
+    createUser(userData, function(user){
+      updateUsersAndView();
+    });
+  });
+}
+
+// Log in an existing user
+function logInUser(usernameAttempt, passwordAttempt, callback){
+  $.ajax({
+    method: 'post',
+    url: '/api/users/authenticate',
+    data: {username: usernameAttempt, password: passwordAttempt},
+    success: function(data) {
+      callback(data);
+    }
+  });
+}
+
+// Log in user form
+function setLogInFormHandler() {
+  $('form#log-in').on('submit', function(e) {
+    e.preventDefault();
+
+    var usernameField = $(this).find('input[name="username"]');
+    var usernameText = usernameField.val();
+    usernameField.varl('');
+
+    var passwordField = $(this).find('input[name="password"]');
+    var passwordText = passwordField.val();
+    passwordField.val('');
+
+    var userData = {username: usernameText, password: passwordText};
+      logInUser(usernameText, passwordText, function(data) {
+
+      $.cookie('token', data.token);
+      updateUsersAndViews();
+    });
+  });
+}
+
+
 function getCities(callback) {
   callback = callback || function(){};
   $.ajax({
