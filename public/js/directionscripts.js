0f5ae6cb6cf8845f5b4412b3181d55765bf2e2cd
ana lins <a.navarrolins@gmail.com>
2016-01-18 17:04:39 -0500

    print directions modal

---
+++ public/js/directionscripts.js
@@ -0,0 +1,30 @@
+console.log('loaded!!');
+
+function DirectionsResults() {
+    $.getJSON('https://maps.googleapis.com/maps/api/directions/json?origin=19060&destination=11226&callback=?', function(data){
+      renderDirections(data);
+      console.log(data);
+    });
+  }
+
+
+function renderDirections(data) {
+  var source = $('#directions').html();
+  var template = Handlebars.compile(source);
+  var compiledHtml = template(data);
+  $('#directions-body').html(compiledHtml);
+}
+
+function printDirections() {
+  $('#print').on('click', function () {
+    var printBody = $('#directions-body').html();
+    $('.container').hide();
+    $('.printable-area').html(printBody);
+    window.print();
+  });
+}
+
+$(function () {
+  DirectionsResults();
+  printDirections();
+});
